89f28af069ae62479f85e90ddb8e3b1885086810
Christian Gaege <christian.gaege@averbis.com>
2019-05-22 17:19:14 +0200

    Added initialize method to generated annotator class

--- src/main/resources/archetype-resources/src/main/java/__annotatorClassName__.java
+++ src/main/resources/archetype-resources/src/main/java/__annotatorClassName__.java
@@ -1,13 +1,24 @@
 package ${package};
 
+import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
 
 public class ${annotatorClassName} extends JCasAnnotator_ImplBase {
 	
 	@Override
+	public void initialize(UimaContext context) throws ResourceInitializationException {
+		
+		super.initialize(context);
+		
+		// add your annotator initialization logic here
+	}
+	
+	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
 		
+		// add your annotator processing logic here
 	}
 }
