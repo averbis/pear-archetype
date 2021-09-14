28692c1741cee6024020b1f9511f8bfbca66cd13
Peter Klügl <peter.kluegl@averbis.com>
2021-09-14 15:48:22 +0200

    Issue #4: Support UIMA 3 - updates for UIMA 3 - extended generated content - fixed and extended test

--- src/main/resources/archetype-resources/src/test/java/PearPackageIT.java
+++ src/main/resources/archetype-resources/src/test/java/PearPackageIT.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Averbis GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ${package};
 
 import java.io.File;
@@ -6,6 +21,7 @@ import java.util.Collection;
 import org.apache.commons.io.FileUtils;
 import org.apache.uima.UIMAFramework;
 import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.cas.CAS;
 import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.fit.util.SimpleNamedResourceManager;
 import org.apache.uima.jcas.JCas;
@@ -37,8 +53,8 @@ public class PearPackageIT {
 
 		analysisEngine.process(jcas);
 
-		Assert.assertEquals("SomeType annotations", 3,
-				cas.select("${package}.SomeType").count());
+		Assert.assertEquals("SomeType annotations", 1,
+				cas.select(SomeType.class).count());
 	}
 
 	private PackageBrowser installPackage() {
--- src/test/resources/projects/it1/reference/src/test/java/com/example/nlp/PearPackageIT.java
+++ src/test/resources/projects/it1/reference/src/test/java/com/example/nlp/PearPackageIT.java
@@ -1,11 +1,27 @@
+/*
+ * Copyright 2021 Averbis GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.nlp;
 
 import java.io.File;
-import java.io.IOException;
+import java.util.Collection;
 
-import org.apache.uima.UIMAException;
+import org.apache.commons.io.FileUtils;
 import org.apache.uima.UIMAFramework;
 import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.cas.CAS;
 import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.fit.util.SimpleNamedResourceManager;
 import org.apache.uima.jcas.JCas;
@@ -13,6 +29,7 @@ import org.apache.uima.pear.tools.PackageBrowser;
 import org.apache.uima.pear.tools.PackageInstaller;
 import org.apache.uima.resource.PearSpecifier;
 import org.apache.uima.util.XMLInputSource;
+import org.junit.Assert;
 import org.junit.Test;
 
 /*
@@ -22,39 +39,34 @@ import org.junit.Test;
  */
 public class PearPackageIT {
 
-	/*
-	 * Make sure that the annotator PEAR package 
-	 * can be installed by the UIMA PackageInstaller
-	 */
 	@Test
-	public void testInstallPearPackage() {
+	public void testProcessText() throws Exception {
 		
-		this.installPackage();			
-	}
-	
-	/*
-	 * Make sure that the annotator is able to 
-	 * process text without exceptions
-	 */
-	@Test
-	public void testProcessText() throws IOException, UIMAException {
-		
-		PackageBrowser packageBrowser = this.installPackage();		
+		PackageBrowser packageBrowser = this.installPackage();
 		XMLInputSource xmlInputSource = new XMLInputSource(packageBrowser.getComponentPearDescPath());
 		PearSpecifier pearSpecifier = UIMAFramework.getXMLParser().parsePearSpecifier(xmlInputSource);
 		AnalysisEngine analysisEngine = UIMAFramework.produceAnalysisEngine(pearSpecifier, new SimpleNamedResourceManager(), null);
 		
-		JCas jCas = JCasFactory.createJCas();
-		jCas.setDocumentText("Test document text");
-		
-		analysisEngine.process(jCas);
+		JCas jcas = JCasFactory.createJCas();
+		CAS cas = jcas.getCas();
+		jcas.setDocumentText("Test document text");
+
+		analysisEngine.process(jcas);
+
+		Assert.assertEquals("SomeType annotations", 1,
+				cas.select(SomeType.class).count());
 	}
 
 	private PackageBrowser installPackage() {
-		
+
 		File installDir = new File("target/generated-test-sources");
-		File pearPackage = new File("target/contract-annotator-0.1.0-SNAPSHOT.pear");
 		
+		Collection<File> files = FileUtils.listFiles(new File("target/"), new String[] { "pear" },
+				false);
+
+		Assert.assertEquals(1, files.size());
+		File pearPackage = files.iterator().next();
+
 		return PackageInstaller.installPackage(installDir, pearPackage, true, false);
 	}
 }
